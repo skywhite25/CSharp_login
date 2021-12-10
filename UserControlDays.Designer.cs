51508cb63b00b35510297cb8db85794515d344b4
TaeHun <skywhite25@naver.com>
2021-12-10 17:30:18 +0900

    calender ing..

---
+++ UserControlDays.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace ReservProgram
+{
+    partial class UserControlDays
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbdays = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // lbdays
+            // 
+            this.lbdays.AutoSize = true;
+            this.lbdays.Location = new System.Drawing.Point(3, 12);
+            this.lbdays.Name = "lbdays";
+            this.lbdays.Size = new System.Drawing.Size(23, 19);
+            this.lbdays.TabIndex = 0;
+            this.lbdays.Text = "00";
+            // 
+            // UserControlDays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.lbdays);
+            this.Name = "UserControlDays";
+            this.Size = new System.Drawing.Size(132, 91);
+            this.Load += new System.EventHandler(this.UserControlDays_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel lbdays;
+    }
+}
