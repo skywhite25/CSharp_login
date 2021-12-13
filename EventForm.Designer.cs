90bf665824cbb7ef68c0a4b2e190d63e7b68d583
TaeHun <skywhite25@naver.com>
2021-12-13 13:07:34 +0900

    calender_EventForm

---
+++ EventForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace ReservProgram
+{
+    partial class EventForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txDate = new System.Windows.Forms.TextBox();
+            this.txEvent = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txDate
+            // 
+            this.txDate.Enabled = false;
+            this.txDate.Location = new System.Drawing.Point(76, 74);
+            this.txDate.Name = "txDate";
+            this.txDate.Size = new System.Drawing.Size(312, 21);
+            this.txDate.TabIndex = 0;
+            // 
+            // txEvent
+            // 
+            this.txEvent.Enabled = false;
+            this.txEvent.Location = new System.Drawing.Point(76, 127);
+            this.txEvent.Name = "txEvent";
+            this.txEvent.Size = new System.Drawing.Size(312, 21);
+            this.txEvent.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Event";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(313, 166);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // EventForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 239);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txEvent);
+            this.Controls.Add(this.txDate);
+            this.Name = "EventForm";
+            this.Text = "일정";
+            this.Load += new System.EventHandler(this.EventForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txDate;
+        private System.Windows.Forms.TextBox txEvent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
---
+++ EventForm.cs
@@ -0,0 +1,37 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ReservProgram
+{
+    public partial class EventForm : MetroForm
+    {
+
+        /*String connString = "server=localhost;user id = root; reservprogram;sslmode=none";*/
+
+
+        public EventForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EventForm_Load(object sender, EventArgs e)
+        {
+            txDate.Text = Setting.static_year +  "/" + Setting.static_month + "/" + UserControlDays.static_day;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+/*            MySqlConnection conn = new MySqlConnection(connString);
+            String sql = "insert into "*/
+        }
+    }
+}
