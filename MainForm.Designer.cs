e6b08b798d467685723a2b263bed227289db0a04
TaeHun <skywhite25@naver.com>
2021-12-14 18:11:11 +0900

    clean

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -31,9 +31,6 @@ namespace ReservProgram
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            DevExpress.XtraScheduler.TimeRuler timeRuler1 = new DevExpress.XtraScheduler.TimeRuler();
-            DevExpress.XtraScheduler.TimeRuler timeRuler2 = new DevExpress.XtraScheduler.TimeRuler();
-            DevExpress.XtraScheduler.TimeRuler timeRuler3 = new DevExpress.XtraScheduler.TimeRuler();
             Telerik.WinControls.UI.RadCalendarDay radCalendarDay1 = new Telerik.WinControls.UI.RadCalendarDay();
             this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.trayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
@@ -42,21 +39,8 @@ namespace ReservProgram
             this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.btnReserve = new MetroFramework.Controls.MetroButton();
             this.btnSetting = new MetroFramework.Controls.MetroButton();
-            this.dashboardBarController1 = new DevExpress.DashboardWin.Bars.DashboardBarController(this.components);
-            this.schedulerDataStorage1 = new DevExpress.XtraScheduler.SchedulerDataStorage(this.components);
-            this.navigationPane1 = new DevExpress.XtraBars.Navigation.NavigationPane();
-            this.navigationPage1 = new DevExpress.XtraBars.Navigation.NavigationPage();
-            this.schedulerControl1 = new DevExpress.XtraScheduler.SchedulerControl();
-            this.navigationPage2 = new DevExpress.XtraBars.Navigation.NavigationPage();
             this.radCalendar2 = new Telerik.WinControls.UI.RadCalendar();
             this.trayMenu.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dashboardBarController1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.schedulerDataStorage1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.navigationPane1)).BeginInit();
-            this.navigationPane1.SuspendLayout();
-            this.navigationPage1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).BeginInit();
-            this.navigationPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.radCalendar2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -120,80 +104,6 @@ namespace ReservProgram
             // 
             // schedulerDataStorage1
             // 
-            // 
-            // 
-            // 
-            this.schedulerDataStorage1.AppointmentDependencies.AutoReload = false;
-            // 
-            // 
-            // 
-            this.schedulerDataStorage1.Appointments.Labels.CreateNewLabel(0, "None", "&None", System.Drawing.SystemColors.Window);
-            this.schedulerDataStorage1.Appointments.Labels.CreateNewLabel(1, "Important", "&Important", System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(194)))), ((int)(((byte)(190))))));
-            this.schedulerDataStorage1.Appointments.Labels.CreateNewLabel(2, "Business", "&Business", System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(213)))), ((int)(((byte)(255))))));
-            this.schedulerDataStorage1.Appointments.Labels.CreateNewLabel(3, "Personal", "&Personal", System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(244)))), ((int)(((byte)(156))))));
-            this.schedulerDataStorage1.Appointments.Labels.CreateNewLabel(4, "Vacation", "&Vacation", System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(228)))), ((int)(((byte)(199))))));
-            this.schedulerDataStorage1.Appointments.Labels.CreateNewLabel(5, "Must Attend", "Must &Attend", System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(206)))), ((int)(((byte)(147))))));
-            this.schedulerDataStorage1.Appointments.Labels.CreateNewLabel(6, "Travel Required", "&Travel Required", System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(244)))), ((int)(((byte)(255))))));
-            this.schedulerDataStorage1.Appointments.Labels.CreateNewLabel(7, "Needs Preparation", "&Needs Preparation", System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(219)))), ((int)(((byte)(152))))));
-            this.schedulerDataStorage1.Appointments.Labels.CreateNewLabel(8, "Birthday", "&Birthday", System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(207)))), ((int)(((byte)(233))))));
-            this.schedulerDataStorage1.Appointments.Labels.CreateNewLabel(9, "Anniversary", "&Anniversary", System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(233)))), ((int)(((byte)(223))))));
-            this.schedulerDataStorage1.Appointments.Labels.CreateNewLabel(10, "Phone Call", "Phone &Call", System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(247)))), ((int)(((byte)(165))))));
-            // 
-            // navigationPane1
-            // 
-            this.navigationPane1.Appearance.BackColor = System.Drawing.Color.Transparent;
-            this.navigationPane1.Appearance.Options.UseBackColor = true;
-            this.navigationPane1.Controls.Add(this.navigationPage1);
-            this.navigationPane1.Controls.Add(this.navigationPage2);
-            this.navigationPane1.Location = new System.Drawing.Point(2, 32);
-            this.navigationPane1.Name = "navigationPane1";
-            this.navigationPane1.Pages.AddRange(new DevExpress.XtraBars.Navigation.NavigationPageBase[] {
-            this.navigationPage1,
-            this.navigationPage2});
-            this.navigationPane1.RegularSize = new System.Drawing.Size(1347, 648);
-            this.navigationPane1.SelectedPage = this.navigationPage1;
-            this.navigationPane1.Size = new System.Drawing.Size(1347, 648);
-            this.navigationPane1.TabIndex = 4;
-            this.navigationPane1.Text = "예약현황/리스트";
-            // 
-            // navigationPage1
-            // 
-            this.navigationPage1.Caption = "일정현황/리스트";
-            this.navigationPage1.Controls.Add(this.schedulerControl1);
-            this.navigationPage1.Name = "navigationPage1";
-            this.navigationPage1.PageText = "예약현황/리스트";
-            this.navigationPage1.Size = new System.Drawing.Size(1203, 575);
-            // 
-            // schedulerControl1
-            // 
-            this.schedulerControl1.ActiveViewType = DevExpress.XtraScheduler.SchedulerViewType.Month;
-            this.schedulerControl1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.schedulerControl1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.schedulerControl1.DataStorage = this.schedulerDataStorage1;
-            this.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.schedulerControl1.Location = new System.Drawing.Point(0, 0);
-            this.schedulerControl1.Name = "schedulerControl1";
-            this.schedulerControl1.Size = new System.Drawing.Size(1203, 575);
-            this.schedulerControl1.Start = new System.DateTime(2021, 12, 12, 0, 0, 0, 0);
-            this.schedulerControl1.TabIndex = 4;
-            this.schedulerControl1.Text = "schedulerControl1";
-            this.schedulerControl1.Views.AgendaView.Enabled = false;
-            this.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1);
-            this.schedulerControl1.Views.FullWeekView.TimeRulers.Add(timeRuler2);
-            this.schedulerControl1.Views.GanttView.Enabled = false;
-            this.schedulerControl1.Views.TimelineView.Enabled = false;
-            this.schedulerControl1.Views.WeekView.Enabled = false;
-            this.schedulerControl1.Views.WorkWeekView.Enabled = false;
-            this.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler3);
-            this.schedulerControl1.Views.YearView.UseOptimizedScrolling = false;
-            // 
-            // navigationPage2
-            // 
-            this.navigationPage2.Caption = "navigationPage2";
-            this.navigationPage2.Controls.Add(this.radCalendar2);
-            this.navigationPage2.Name = "navigationPage2";
-            this.navigationPage2.Size = new System.Drawing.Size(1203, 575);
-            // 
             // radCalendar2
             // 
             this.radCalendar2.HeaderHeight = 28;
@@ -209,30 +119,6 @@ namespace ReservProgram
             this.radCalendar2.TabIndex = 1;
             this.radCalendar2.ThemeName = "Fluent";
             this.radCalendar2.SelectionChanged += new System.EventHandler(this.radCalendar2_SelectionChanged);
-            // 
-            // MainForm
-            // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
-            this.BorderStyle = MetroFramework.Drawing.MetroBorderStyle.FixedSingle;
-            this.ClientSize = new System.Drawing.Size(1343, 823);
-            this.Controls.Add(this.navigationPane1);
-            this.Controls.Add(this.btnSetting);
-            this.Controls.Add(this.btnReserve);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "MainForm";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
-            this.Load += new System.EventHandler(this.MainForm_Load);
-            this.trayMenu.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dashboardBarController1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.schedulerDataStorage1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.navigationPane1)).EndInit();
-            this.navigationPane1.ResumeLayout(false);
-            this.navigationPage1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).EndInit();
-            this.navigationPage2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.radCalendar2)).EndInit();
-            this.ResumeLayout(false);
 
         }
 
@@ -245,12 +131,6 @@ namespace ReservProgram
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
         private MetroFramework.Controls.MetroButton btnReserve;
         private MetroFramework.Controls.MetroButton btnSetting;
-        private DevExpress.DashboardWin.Bars.DashboardBarController dashboardBarController1;
-        private DevExpress.XtraScheduler.SchedulerDataStorage schedulerDataStorage1;
-        private DevExpress.XtraBars.Navigation.NavigationPane navigationPane1;
-        private DevExpress.XtraBars.Navigation.NavigationPage navigationPage1;
-        private DevExpress.XtraScheduler.SchedulerControl schedulerControl1;
-        private DevExpress.XtraBars.Navigation.NavigationPage navigationPage2;
         private Telerik.WinControls.UI.RadCalendar radCalendar2;
     }
 }
